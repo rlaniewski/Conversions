e82b702f90b10eaf7fa0cdc66cfee75ab35f0c20
rlaniewski <depthn@gmail.com>
2020-07-17 14:13:25 +0200

    Added ktoc (kelvin to celsius) endpoint.

--- src/main/java/com/conversions/resources/ConversionsResource.java
+++ src/main/java/com/conversions/resources/ConversionsResource.java
@@ -1,11 +1,20 @@
 package com.conversions.resources;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("/conversions")
 /**
  * A REST web service that provides endpoints for celsius/kelvin and kilometer/mile conversions.
  */
 public class ConversionsResource {
+
+    @GET
+    @Path("/ktoc/{value}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Float ktoc(@PathParam("value") float kelvin) {
+        return kelvin - 273.15f;
+    }
 
 }
