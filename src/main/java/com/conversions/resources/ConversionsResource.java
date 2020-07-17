846e8d3678b083b61ec1f6bc4ed9650951a533da
rlaniewski <depthn@gmail.com>
2020-07-17 14:10:09 +0200

    Added ConversionsResource (REST web service) class outline.

---
+++ src/main/java/com/conversions/resources/ConversionsResource.java
@@ -0,0 +1,11 @@
+package com.conversions.resources;
+
+import javax.ws.rs.*;
+
+@Path("/conversions")
+/**
+ * A REST web service that provides endpoints for celsius/kelvin and kilometer/mile conversions.
+ */
+public class ConversionsResource {
+
+}
